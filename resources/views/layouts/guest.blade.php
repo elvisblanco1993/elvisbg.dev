e7d5cccf911226e6495cb82b3aef9e402b8cc0d7
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2024-08-08 16:53:30 -0400

    ..

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -3,7 +3,7 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
+        {{-- <meta name="csrf-token" content="{{ csrf_token() }}"> --}}
 
         <title>{{ config('app.name', 'Laravel') }}</title>
 
@@ -12,9 +12,9 @@
         <link href="https://fonts.bunny.net/css?family=inter:100,200,300,400,500,600,700,800,900" rel="stylesheet" />
 
         <!-- Fathom - beautiful, simple website analytics -->
-        {{-- @if (request()->routeIs('login') && request()->routeIs('register'))
+        @if (request()->routeIs('login') && request()->routeIs('register'))
             <script src="https://cdn.usefathom.com/script.js" data-site="SBIGASGI" defer></script>
-        @endif --}}
+        @endif
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
